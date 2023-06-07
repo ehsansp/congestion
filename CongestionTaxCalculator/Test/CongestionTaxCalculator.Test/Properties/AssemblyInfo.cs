5b1d26337eb46a925a077e939caa27eb9d07a9c9
Ehsan <Ehsan@DESKTOP-JBTTBLE>
2023-06-07 11:49:11 +0330

    Create Test Project

---
+++ CongestionTaxCalculator/Test/CongestionTaxCalculator.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CongestionTaxCalculator.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CongestionTaxCalculator.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f88d5ee7-b922-40d9-82d5-8abf91412078")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
